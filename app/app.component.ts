e6ce2ee48bf32fcbfb98598091688961ab42b30c
Stephen Emery <emery.stephen@gmail.com>
2016-10-21 09:04:49 -0700

    Add app.component.ts

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'calorie-keeper',
+  template: `
+    <div class="container">
+      <div class="jumbotron text-center">
+        <h1>Calorie Keeper</h1>
+      </div>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
